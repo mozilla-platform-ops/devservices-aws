e77170fe3f140501a0bb84f7ad04e8ffacff822b
Ed Morley <edmorley@users.noreply.github.com>
2017-07-07 17:35:55 +0100

    Add some tests and a Travis configuration
    This validates JSON files, shell scripts and Terraform configs.
    
    Inspired by Travis's own Terraform repo tests script:
    https://github.com/travis-infrastructure/terraform-config/blob/7ed91676a42e17e2e6d03ecaa699c3f615acd4bd/runtests#L43
    
    In the future `terraform fmt` could be added to the checks run here.
    
    Fixes #54 / https://bugzilla.mozilla.org/show_bug.cgi?id=1379211

---
+++ runtests.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# This is inspired by:
+# https://github.com/travis-infrastructure/terraform-config/blob/master/runtests
+
+set -euo pipefail
+
+main() {
+  echo -e '\n-----> Validating JSON'
+  for f in $(git ls-files '*.json'); do
+    echo -en "$f "
+    python -m json.tool <"${f}" >/dev/null
+    echo "✓"
+  done
+
+  echo -e '\n-----> Running shellcheck'
+  git grep -El '^#!/.+\b(bash|sh)\b' -- './*' ':(exclude)*.tmpl' | xargs shellcheck
+
+  echo -e '\n-----> Running terraform validate'
+  for d in $(git ls-files '*.tf' | xargs -n1 dirname | LC_ALL=C sort | uniq); do
+    echo -en "${d} "
+    terraform validate "${d}"
+    echo "✓"
+  done
+
+  echo -e '\n-----> Success!'
+}
+
+main "$@"
