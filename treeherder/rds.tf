7b6dc7a8b4a2370f4a0721b0a9d2eef439c7ba90
Jake Watkins <jwatkins@mozilla.com>
2019-09-09 07:47:33 -0700

    [treeherder] Downgrade treeherder-stage and remove multi-az; 1577012

--- treeherder/rds.tf
+++ treeherder/rds.tf
@@ -110,11 +110,11 @@ resource "aws_db_instance" "treeherder-stage-rds" {
     allocated_storage = 1000
     engine = "mysql"
     engine_version = "5.7.23"
-    instance_class = "db.m5.2xlarge"
+    instance_class = "db.m5.xlarge"
     backup_retention_period = 1
     backup_window = "07:00-07:30"
     maintenance_window = "Sun:08:00-Sun:08:30"
-    multi_az = true
+    multi_az = false
     port = "3306"
     publicly_accessible = true
     parameter_group_name = "${aws_db_parameter_group.treeherder-pg-mysql57.name}"
