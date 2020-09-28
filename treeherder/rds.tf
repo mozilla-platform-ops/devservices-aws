b83c78d520ca81504d39d51c31ac4befc15a3301
davehouse <dhouse@mozilla.com>
2020-09-28 11:37:23 -0600

    match current setting for performance_insights

--- treeherder/rds.tf
+++ treeherder/rds.tf
@@ -209,6 +209,7 @@ resource "aws_db_instance" "treeherder-dev-rds" {
     vpc_security_group_ids = ["${aws_security_group.treeherder_heroku-sg.id}"]
     monitoring_role_arn = "arn:aws:iam::699292812394:role/rds-monitoring-role"
     monitoring_interval = 60
+    performance_insights_enabled = true
     tags {
         Name = "treeherder-dev-rds"
         App = "treeherder"
@@ -244,6 +245,7 @@ resource "aws_db_instance" "treeherder-stage-rds" {
     vpc_security_group_ids = ["${aws_security_group.treeherder_heroku-sg.id}"]
     monitoring_role_arn = "arn:aws:iam::699292812394:role/rds-monitoring-role"
     monitoring_interval = 60
+    performance_insights_enabled = true
     tags {
         Name = "treeherder-stage-rds"
         App = "treeherder"
@@ -279,6 +281,7 @@ resource "aws_db_instance" "treeherder-prod-rds" {
     vpc_security_group_ids = ["${aws_security_group.treeherder_heroku-sg.id}"]
     monitoring_role_arn = "arn:aws:iam::699292812394:role/rds-monitoring-role"
     monitoring_interval = 60
+    performance_insights_enabled = true
     tags {
         Name = "treeherder-prod-rds"
         App = "treeherder"
@@ -306,6 +309,7 @@ resource "aws_db_instance" "treeherder-prod-ro-rds" {
     vpc_security_group_ids = ["${aws_security_group.treeherder_heroku-sg.id}"]
     monitoring_role_arn = "arn:aws:iam::699292812394:role/rds-monitoring-role"
     monitoring_interval = 60
+    performance_insights_enabled = false
     tags {
         Name = "treeherder-prod-ro-rds"
         App = "treeherder"
